63a3434c287ff05e03f8a8c8ccdb32e59516cf0a
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-09-08 22:52:01 +0300

    test: configure browser tests

---
+++ playwright.config.js
@@ -0,0 +1,2 @@
+import {defineConfig} from '@playwright/test';
+export default defineConfig({testDir:'./tests',use:{baseURL:'http://127.0.0.1:4173'},webServer:{command:'npx http-server . -p 4173 -c-1',port:4173,reuseExistingServer:true},reporter:'line'});
